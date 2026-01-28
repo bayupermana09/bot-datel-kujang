cd0fc086bf0d4f48ae077ac7a87ae7635ba57812
bayupermana <bayupermana78910@gmail.com>
2026-01-28 13:33:51 +0700

    fix auth googlr

--- tes.js
+++ tes.js
@@ -21,7 +21,7 @@ const sheetName = "Data Visit";
 const auth = new google.auth.GoogleAuth({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    private_key: process.env.GOOGLE_KEY.replace(/\\n/g, "\n"),
   },
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
